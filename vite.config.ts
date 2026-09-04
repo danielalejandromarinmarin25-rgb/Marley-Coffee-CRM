1bc1682e871768beecb1c83b3b6b34c24a450258
14 <kvtorce@MacBook-Air-de-14.local>
2026-09-03 20:19:32 -0400

    ci: deploy app to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Marley-Coffee-CRM/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
